e08ead4d30c1727637e9aaa4066fa158ba7c7779
Drozdetskiy <m.drozdetskiy@gmail.com>
2019-02-26 18:59:37 +0300

    Last Task (task 6)

---
+++ homework9/My_homework_9_6.py
@@ -0,0 +1,29 @@
+class BinomialCoefficients:
+    def __init__(self, n):
+        self.n = n+1
+        self._prev_line = [0 for i in range(n+1)]
+        self._new_line = []
+        self.index1 = 0
+        self.index2 = 0
+
+    def __next__(self):
+        while self.index1 < self.n:
+            _res = (self._prev_line[self.index2-1] + self._prev_line[self.index2]) if self.index2 else 1
+            self._new_line.append(_res)
+            self.index2 += 1
+            if self.index2 == self.n:
+                self._prev_line, self._new_line = self._new_line, []
+                self.index2 = 0
+                self.index1 += 1
+            if (self.index1 >= self.n-1) and _res:
+                return _res
+        raise StopIteration
+
+    def __iter__(self):
+        return self
+
+
+if __name__ == '__main__':
+    a = BinomialCoefficients(5)
+    for index2 in a:
+        print(index2)
