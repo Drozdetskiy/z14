0448aa0040de3e8de70f5ca386e5497e0e5672fd
Drozdetskiy <m.drozdetskiy@gmail.com>
2019-02-26 19:08:04 +0300

    microfixes

--- homework9/My_homework_9_6.py
+++ homework9/My_homework_9_6.py
@@ -1,20 +1,25 @@
 class BinomialCoefficients:
     def __init__(self, n):
         self.n = n+1
-        self._prev_line = [0 for i in range(n+1)]
+        self._prev_line = [0 for _ in range(n+1)]
         self._new_line = []
         self.index1 = 0
         self.index2 = 0
 
     def __next__(self):
         while self.index1 < self.n:
-            _res = (self._prev_line[self.index2-1] + self._prev_line[self.index2]) if self.index2 else 1
+            _res = (self._prev_line[self.index2-1] +
+                    self._prev_line[self.index2]) \
+                if self.index2 else 1
+
             self._new_line.append(_res)
             self.index2 += 1
+
             if self.index2 == self.n:
                 self._prev_line, self._new_line = self._new_line, []
                 self.index2 = 0
                 self.index1 += 1
+
             if (self.index1 >= self.n-1) and _res:
                 return _res
         raise StopIteration
